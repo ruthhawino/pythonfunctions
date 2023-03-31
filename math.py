53ef41221a4e6001a09d20b1de03288bd4cbaa44
ruthhawino <awinoruthh@gmail.com>
2023-03-31 16:13:12 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def subtract(a,b):
+        answer = a-b
+        return answer
+
+def multiply(a,b):
+    answer = a*b
+    return answer
+
+def modulas(a,b):
+    answer = a%b
+    return answer
+
+def  division(a,b):
+     answer =a/b
+     return answer
